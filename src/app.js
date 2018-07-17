963af37e9bd97b61f3e15d41591ff2c1da323aaa
Irma <irblancofdz@gmail.com>
2018-07-17 16:58:38 +0200

    Use webpack with react

--- src/app.js
+++ src/app.js
@@ -1,18 +1,9 @@
-//named imports
-// import substract,  {  square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const template = <p>This is jsx from webpack</p>
+ReactDOM.render(template, document.getElementById('app'));
 
-
-console.log('app.js is running');
-// console.log(square(2));
-// console.log(add(100, 22));
-// console.log(substract(8, 3));
-
-import isSenior, {  isAdult, canDrink } from './person.js';
-console.log(isSenior(50));
-console.log(isSenior(70));
-// console.log(isAdult(19));
-// console.log(canDrink(19));
 
 
 
